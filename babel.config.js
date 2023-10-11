5c78ae4169dc21aa5133322ecae65fb4c4acb091
yemomo <1463645455@qq.com>
2023-10-11 22:26:50 +0800

    feat: 配置路径别名

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // 配置 babel-plugin-module-resolver
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+        alias: {
+          '@': ['./src'],
+          '@assets': './src/assets',
+          '@components': './src/Components',
+          '@Screen': './src/Screen',
+        },
+      },
+    ],
+  ],
 };
